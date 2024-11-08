c3821f0328d0f36cc318a6431ab4f5aee097cff5
Thai Chau <tc441985@gmail.com>
2024-11-08 07:25:42 +0700

    create user model

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('Connected to MongoDB');
+}).catch(e => {
+    console.error('Error connecting to MongoDB:', e);
+})
 
 const app = express();
 
